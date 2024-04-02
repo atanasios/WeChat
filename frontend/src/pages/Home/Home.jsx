a007d862ed928944257f88307db2c0bf7734a943
Atanas <atanasgeorgiev444@gmail.com>
2024-04-02 12:18:41 +0300

    feat: Implement Home component
    - Created Home component to serve as the main page of the application
    - Includes Sidebar component for displaying user list
    - Includes MessageContainer component for displaying messages
    
    The Home component provides the main interface for the application, displaying the sidebar with user list and the message container. It is designed to be responsive and visually appealing.

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import MessageContainer from "../../components/Messages/MessageContainer";
+
+const Home = () => {
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <Sidebar />
+        <MessageContainer />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
